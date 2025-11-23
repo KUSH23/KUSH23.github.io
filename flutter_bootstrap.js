139081863e6ddbed701b2a9005690641eacb175d
KUSH23 <kushalsai2007@gmail.com>
2025-11-24 02:07:39 +0530

    updated cv link

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"29a2f674ca2312e34a3ef796701c297dc3be1a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1777558947"
+    serviceWorkerVersion: "3578095913"
   }
 });
